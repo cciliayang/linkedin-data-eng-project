26e1c099ab34c32e19af9b412015a8840e3f239e
cciliayang-github <cesse.yang@gmail.com>
2024-08-29 11:57:06 -0400

    updated job_market_analysis file

--- dbt_linkedin_project/models/marts/job_market/job_market_analysis.sql
+++ dbt_linkedin_project/models/marts/job_market/job_market_analysis.sql
@@ -1,12 +1,26 @@
--- Final Model for analyzing job market
+-- Model for analyzing the job market
+-- use join tables and aggregate the data to get number of job postings
+WITH job_postings_aggregated AS (
+    SELECT
+        l.job_location AS location,
+        jt.job_title_name AS job_title,
+        COUNT(*) AS number_of_postings
+    FROM
+        {{ ref('fact_job_posting') }} f
+    INNER JOIN
+        {{ ref('dim_location') }} l ON f.location_id = l.location_id
+    INNER JOIN
+        {{ ref('dim_job_title') }} jt ON f.job_title_id = jt.job_title_id
+    GROUP BY
+        l.job_location,
+        jt.job_title_name
+)
 
 SELECT
     job_title,
     location,
     number_of_postings
---FROM
-    --{{ ref('int_job_posting')}}
- FROM
-        {{ source('transformed_job_posting_data', 'int_job_posting') }}
+FROM
+    job_postings_aggregated
 ORDER BY
     number_of_postings DESC
